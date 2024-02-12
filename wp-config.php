0b61e350e2642afd813d04b766c493f76e187722
NDULZ <shaq32bball@live.com>
2024-02-12 21:59:00 +0300

    Config Redux

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'shaeteqc_wp828' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'shaeteqc_wp828' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', '9.]5XS249p' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
